bcf53e8e918ba9beed1c6aea004f227c38cecae4
Aleksey <alekseykarpelev@gmail.com>
2022-09-26 13:14:31 +1000

    add method search

--- app/Http/Controllers/Api/SearchPageController.php
+++ app/Http/Controllers/Api/SearchPageController.php
@@ -4,11 +4,43 @@ namespace BookStack\Http\Controllers\Api;
 
 use BookStack\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use BookStack\Entities\Models\Page;
 
 class SearchPageController extends ApiController
 {
-    public function  search()
+    public function  search(Request $request): \Illuminate\Http\JsonResponse
     {
-        echo 'hi';
+        $class = $request['class'];
+        $type  = $request['type'];
+        $word  = $request['word'];
+
+        $output = [];
+        $result = [];
+        $output['input']= [
+            'class' => $request['class'],
+            'type'  => $request['type'],
+            'word'  => $request['word'],
+        ];
+        $i =  0 ;
+
+        foreach (Page::all() as $page) {
+
+          //  $output[$i] = $page->name;
+
+            $output['result'][$i] = [
+                'page_title' => $page->name,
+                'page_id'   => $page->id,
+                'data' => [],
+
+            ];
+
+            $i++;
+        }
+
+//dd($output);
+
+
+       return response()->json($output);
+
     }
 }
